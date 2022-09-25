7e3966a7360871e11d0aa9aa2dc4563f8b771e91
geroitrueda <91047066+geroitrueda@users.noreply.github.com>
2022-09-26 00:22:14 +0500

    Create JunitExtension.java

---
+++ src/test/java/ru/gb/lesson7/JunitExtension.java
@@ -0,0 +1,23 @@
+package ru.gb.lesson7;
+
+import io.qameta.allure.Allure;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.TestWatcher;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import java.io.ByteArrayInputStream;
+
+public class JunitExtension implements TestWatcher {
+    ByteArrayInputStream screenshot;
+    public ByteArrayInputStream getScreenshot() {
+    return screenshot;
+}
+    public void setScreenshot(ByteArrayInputStream screenshot) {
+        this.screenshot = screenshot;
+    }
+    public void testAborted(ExtensionContext context, Throwable cause) {
+        Allure.addAttachment("Скриншот перед закрытием браузера", screenshot);
+    }
+}
